ad7df954d064ed191c66dced010a4ae1478b59b1
ashra325 <86569956+ashra325@users.noreply.github.com>
2021-07-08 23:01:41 +0530

    Add files via upload

---
+++ ASCIIValue_5.java
@@ -0,0 +1,12 @@
+package knightingale;
+
+public class ASCIIValue_5 {
+		 public static void main(String[] args) { 
+		 char ch = 'H'; 
+		 int ascii = ch; 
+		 // You can also cast char to int
+		int castAscii = (int) ch; 
+		 System.out.println("The ASCII value of " + ch + " is: " + ascii); 
+		 System.out.println("The ASCII value of " + ch + " is: " + castAscii); 
+		 } 
+}
